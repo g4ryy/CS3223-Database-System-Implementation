9c519cf0eb42278b626733ee0cab95012b7d1648
G <garyyansen@gmail.com>
2022-02-04 22:33:22 +0800

    CreateIndexData: Add idxType field and getter

--- SimpleDBEngine/src/simpledb/parse/CreateIndexData.java
+++ SimpleDBEngine/src/simpledb/parse/CreateIndexData.java
@@ -5,15 +5,16 @@ package simpledb.parse;
  * @author Edward Sciore
  */
 public class CreateIndexData {
-   private String idxname, tblname, fldname;
+   private String idxname, tblname, fldname, idxType;
    
    /**
     * Saves the table and field names of the specified index.
     */
-   public CreateIndexData(String idxname, String tblname, String fldname) {
+   public CreateIndexData(String idxname, String tblname, String fldname, String idxType) {
       this.idxname = idxname;
       this.tblname = tblname;
       this.fldname = fldname;
+      this.idxType = idxType;
    }
    
    /**
@@ -38,6 +39,14 @@ public class CreateIndexData {
     */
    public String fieldName() {
       return fldname;
+   }
+
+   /**
+    * Returns the type of the indexed field.
+    * @return the type of the indexed field
+    */
+   public String indexType() {
+      return idxType;
    }
 }
 
