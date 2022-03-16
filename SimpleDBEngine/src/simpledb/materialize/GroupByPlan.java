a55e3121a3219f765600646efa607eb7be1db9a3
G <garyyansen@gmail.com>
2022-03-16 12:54:34 +0800

    Fix bug in groupbyplan

--- SimpleDBEngine/src/simpledb/materialize/GroupByPlan.java
+++ SimpleDBEngine/src/simpledb/materialize/GroupByPlan.java
@@ -31,6 +31,7 @@ public class GroupByPlan implements Plan {
     * @param tx the calling transaction
     */
    public GroupByPlan(Transaction tx, Plan p, List<String> groupfields, List<AggregationFn> aggfns) {
+      this.originalPlan = p;
       this.p = new SortPlan(tx, p, groupfields.stream()
             .map(fld -> new OrderField(fld, "asc")).collect(Collectors.toList()), false);
       this.groupfields = groupfields;
